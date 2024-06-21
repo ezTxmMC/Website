862c57719e6ba98d49ebfe56ced07b601bfedd5d
ezTxmMC <eztxmmc@hotmail.com>
2024-06-21 16:53:44 +0200

    feat: added basics

---
+++ home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ezTxmMC</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <div class="left">
+
+        </div>
+        <div class="right">
+
+        </div>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        <?php echo date("Y"); ?>
+    </footer>
+</body>
+
+</html>
